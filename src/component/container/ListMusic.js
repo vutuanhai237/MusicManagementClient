1401069b80c55bb78e0c387c6f17bb9641ca416d
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2020-10-19 16:46:18 +0700

    fetch API get all musics

--- src/component/container/ListMusic.js
+++ src/component/container/ListMusic.js
@@ -1,59 +1,78 @@
 import React, { useEffect, useState } from "react";
-import { Col, Row, Table } from "react-bootstrap";
+import { Col, Row, Table, Button } from "react-bootstrap";
 import { PAGE_HOME_TITLE } from "../../constant/index";
 import { useSelector, useDispatch } from 'react-redux';
 import { getMusicAPI } from '../../service/MusicService'
-import {HOST, PORT} from '../../constant'
+import { AiOutlineEdit, AiOutlineDelete, AiOutlineFileAdd } from 'react-icons/ai'
+import { HOST, PORT } from '../../constant'
+import axios from 'axios';
 import "./ListMusic.scss";
 const ListMusic = (props) => {
-    let [musics, setMusices] = useState([])
-
+    const [musics, setMusices] = useState([])
     useEffect(() => {
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow',
-          };
-          
-          fetch(`http://${HOST}:${PORT}/musics`, requestOptions)
-            .then(response => {
-                response.text()
-            })
-            .then(result => {
-                console.log(1111111)
-                console.log((result))
-            })
-            .catch(error => console.log('error', error));
-    })
+        async function fetch() {
+            const result = await axios(`http://${HOST}:${PORT}/musics`)
+            setMusices(result.data);
+        }
+        fetch()
+    }, [])
 
-    // const { musics } = this.props;
+    function deleteMusic (id) {
+        console.log(id)
+    }
+
+    function addMusic () {
+
+    }
+
+    function modifyMusic () {
+
+    }
+
     return (
-        <div id="pageHome">
+        <div id="listMusic">
+            <Button onClick={() => { console.log(musics) }}>Click</Button>
             <Col>
                 <Row>
                     <p id="pageTitle">{PAGE_HOME_TITLE}</p>
+
                 </Row>
                 <Row>
                     <Table responsive>
                         <thead>
                             <tr>
                                 <th>Song name</th>
+                                <th>Release time</th>
                                 <th>Genre</th>
                                 <th>Musican</th>
                                 <th>Singer</th>
+                                <th><Button variant="success">
+                                    <AiOutlineFileAdd />
+                                </Button></th>
                             </tr>
                         </thead>
                         <tbody>
-                            {/* {
+                            {
                                 musics.map(e => {
-                                    return <tr>
+                                    return <tr key={e.id}>
                                         <td>{e.name}</td>
                                         <td>{e.releaseTime}</td>
-                                        <td>{e.musican}</td>
                                         <td>{e.genre.name}</td>
+                                        <td>{e.musican.name}</td>
+                                        <td>{e.singer.name}</td>
+                                        <td>
+                                            <Button className="modifyButton" onClick={()=> modifyMusic()} variant="warning">
+                                                <AiOutlineEdit />
+                                            </Button>
+                                            <Button className="deleteButton" onClick={()=> deleteMusic(e.id)} variant="danger">
+                                                <AiOutlineDelete />
+                                            </Button>
+                                        </td>
                                     </tr>
                                 })
-                            } */}
+                            }
                         </tbody>
+
                     </Table>
                 </Row>
             </Col>
