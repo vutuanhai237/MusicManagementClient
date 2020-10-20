7c90c0a7e1283938c1590ae277c415fe28b5b399
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2020-10-20 17:38:21 +0700

    add ref to proceed Add music function

---
+++ src/component/MusicManagement/MusicModal.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import {Button, Modal, Form } from "react-bootstrap";
+const MusicModal = (props) => {
+    const [show, setShow] = useState(true);
+    
+    return (
+        
+        <div></div>
+
+    )
+}
+
+
+
+export default MusicModal;
--- src/component/container/ListMusic.js
+++ src/component/container/ListMusic.js
@@ -1,14 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { Col, Row, Table, Button } from "react-bootstrap";
+import React, { useEffect, useState, useRef } from "react";
+import { Col, Row, Table, Button, Modal, Form } from "react-bootstrap";
 import { PAGE_HOME_TITLE } from "../../constant/index";
-import { useSelector, useDispatch } from 'react-redux';
-import { getMusicAPI } from '../../service/MusicService'
 import { AiOutlineEdit, AiOutlineDelete, AiOutlineFileAdd } from 'react-icons/ai'
 import { HOST, PORT } from '../../constant'
 import axios from 'axios';
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import "./ListMusic.scss";
 const ListMusic = (props) => {
+    // state
     const [musics, setMusices] = useState([])
+    const [show, setShow] = useState(false);
+    const [nameMusic, setNameMusic] = useState("");
+    const [date, setDate] = useState(new Date());
+    const [genre, setGenre] = useState("");
+    const [musician, setMusician] = useState("");
+    const [singer, setSinger] = useState("");
+    // ref
+    const datepickerRef = useRef(null);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    // properties
+    const listGenre = [{id:1, name: "A"}, {id:2, name: "B"}]
+    const listMusician = [{id:1, name: "A"}, {id:2, name: "B"}]
+    const listSinger = [{id:1, name: "A"}, {id:2, name: "B"}]
     useEffect(() => {
         async function fetch() {
             const result = await axios(`http://${HOST}:${PORT}/musics`)
@@ -17,21 +32,88 @@ const ListMusic = (props) => {
         fetch()
     }, [])
 
-    function deleteMusic (id) {
+    const deleteMusic = (id) => {
         console.log(id)
     }
 
-    function addMusic () {
+    const addMusic = () => {
+        // name, releaseTime, genreID, musicianID, singerID
 
+        console.log(nameMusic)
+        console.log(date)
+        console.log(genre)
+        console.log(musician)
+        console.log(singer)
+        handleClose()
     }
 
-    function modifyMusic () {
+    const modifyMusic = () => {
 
     }
 
     return (
+
         <div id="listMusic">
-            <Button onClick={() => { console.log(musics) }}>Click</Button>
+            <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add music</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group controlId="nameArea">
+                        <Form.Label>Music's name</Form.Label>
+                        <Form.Control value={nameMusic} onChange={e => setNameMusic(e.target.value)}
+                            as="textarea" placeholder="Enter name" />
+
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Release time</Form.Label>
+                        <br></br>
+                        <ReactDatePicker selected={date}
+                            onChange={setDate} ref={datepickerRef} id="datePicker" />
+                    </Form.Group>
+                    <Form.Group controlId="genreArea">
+                        <Form.Label>Genre</Form.Label>
+                        <Form.Control value={genre} onChange={e => setGenre(e.target.value)} as="select">
+                            {
+                                listGenre.map(e => {
+                                    return <option key={e.id}> {e.name} </option>
+                                })
+                            }
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="musicianArea">
+                        <Form.Label>Musician</Form.Label>
+                        <Form.Control value={musician} onChange={e => setMusician(e.target.value)} as="select">
+                            {
+                                listMusician.map(e => {
+                                    return <option key={e.id}> {e.name} </option>
+                                })
+                            }
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="singerArea">
+                        <Form.Label>Singer</Form.Label>
+                        <Form.Control value={singer} onChange={e => setSinger(e.target.value)} as="select">
+                            {
+                                listSinger.map(e => {
+                                    return <option key={e.id}> {e.name} </option>
+                                })
+                            }
+                        </Form.Control>
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="success" onClick={addMusic}>
+                        Add
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+
+            <Button onClick={() => { console.log(nameMusic) }}>Click</Button>
             <Col>
                 <Row>
                     <p id="pageTitle">{PAGE_HOME_TITLE}</p>
@@ -46,7 +128,7 @@ const ListMusic = (props) => {
                                 <th>Genre</th>
                                 <th>Musican</th>
                                 <th>Singer</th>
-                                <th><Button variant="success">
+                                <th><Button variant="success" onClick={handleShow}>
                                     <AiOutlineFileAdd />
                                 </Button></th>
                             </tr>
@@ -61,10 +143,10 @@ const ListMusic = (props) => {
                                         <td>{e.musican.name}</td>
                                         <td>{e.singer.name}</td>
                                         <td>
-                                            <Button className="modifyButton" onClick={()=> modifyMusic()} variant="warning">
+                                            <Button className="modifyButton" onClick={() => modifyMusic()} variant="warning">
                                                 <AiOutlineEdit />
                                             </Button>
-                                            <Button className="deleteButton" onClick={()=> deleteMusic(e.id)} variant="danger">
+                                            <Button className="deleteButton" onClick={() => deleteMusic(e.id)} variant="danger">
                                                 <AiOutlineDelete />
                                             </Button>
                                         </td>
